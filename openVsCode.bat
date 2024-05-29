17b804924aec4e4c40157e58b67f0afdf978cc44
sakibb7 <sakibb076@gmail.com>
2024-05-29 16:55:17 +0600

    updated packeges, added oneclick open vs code and run project file

---
+++ openVsCode.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd.exe /k "code ."
+exit
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('fs');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackSimpleIncludePlugin = require("html-webpack-simple-include-plugin");
 module.exports = {
@@ -12,23 +12,19 @@ module.exports = {
   devServer: {
     watchFiles: ["src/**/*"],
     hot: true,
-    port:8080,
+    port: 8080,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         include: path.resolve(__dirname, "src"),
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        type: "asset/resource",
+      },
     ],
   },
   resolve: {
@@ -36,24 +32,27 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: "style.css",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: "src/assets", to: "assets" }
-      ],
+      patterns: [{ from: "src/assets", to: "assets" }],
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
+    new HtmlWebpackPlugin({
+      filename: "testPage.html",
+      template: "src/testPage.html",
+    }),
 
     new HtmlWebpackSimpleIncludePlugin([
       {
-        tag: '<include-check />',
-        content: fs.readFileSync(path.resolve(__dirname, "src/partials/check.html")),
-
+        tag: "<include-check />",
+        content: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/check.html")
+        ),
       },
-    ])
+    ]),
   ],
   output: {
     filename: "index.js",
